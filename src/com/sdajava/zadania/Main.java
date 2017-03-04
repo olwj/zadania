201f191b0a949131217a1d79aa59f09b6e647028
olwj <al.wajgert@gmail.com>
2017-03-04 13:17:12 +0100

    add final files

---
+++ src/com/sdajava/zadania/Main.java
@@ -0,0 +1,11 @@
+package com.sdajava.zadania;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Pola kwadrat = new Pola (4, 5);
+        System.out.println(kwadrat.polekwadratu());
+    }
+
+}
